41c13ffeef3bb9c7b2fce0032f085ec04803fa71
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-11-15 20:27:44 -0500

    added for exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,40 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.print("Hello");
+//        System.out.print(" World!");
+//        int myFavoriteNumber = 7;
+//        System.out.println();
+//        System.out.println(myFavoriteNumber);
+//        String myString = "Testing";
+//        System.out.println(myString);
+//        float myNumber = 3.14f;
+//
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+//        x =+5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        x /=y;
+//        y = y - x;
+//        y -= x;
+    }
+}
