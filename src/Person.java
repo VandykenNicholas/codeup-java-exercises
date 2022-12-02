4f71a8e64fece12dbd0bd8c24a242bd661ae5b9a
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-12-02 00:10:15 -0500

    added for exercise

--- src/Person.java
+++ src/Person.java
@@ -18,28 +18,28 @@ public class Person {
     }
     public void sayHello(){
 //TODO: print a message to the console using the person's name
-        System.out.println("Hello, from, " + this.name);
+        System.out.println("Hello, from " + this.name);
     }
 
-    public static void main (String[] args){
-//        Person person1 = new Person("John");
-//        Person person2 = new Person("John");
-//        System.out.println(person1.getName().equals(person2.getName()));
-//        System.out.println(person1 == person2);
-
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1 == person2);
-
-//        Person person1 = new Person("John");
-//        Person person2 = new Person("John");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-//        person2.setName("Jane");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-
-    }
+//    public static void main (String[] args){
+////        Person person1 = new Person("John");
+////        Person person2 = new Person("John");
+////        System.out.println(person1.getName().equals(person2.getName()));
+////        System.out.println(person1 == person2);
+//
+////        Person person1 = new Person("John");
+////        Person person2 = person1;
+////        System.out.println(person1 == person2);
+//
+////        Person person1 = new Person("John");
+////        Person person2 = new Person("John");
+////        System.out.println(person1.getName());
+////        System.out.println(person2.getName());
+////        person2.setName("Jane");
+////        System.out.println(person1.getName());
+////        System.out.println(person2.getName());
+//
+//    }
 
 
 }
